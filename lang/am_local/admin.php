2ef2e96a6eab3f380a7a22d415154b7b3f101e79
Natnael Tewodros <natnaeltewodros03@gmail.com>
2026-08-18 21:52:35 +0300

    feat(i18n): batch-translate core and activity language files into Amharic

--- lang/am_local/admin.php
+++ lang/am_local/admin.php
@@ -32,9 +32,13 @@ $string['browseusers'] = 'የተጠቃሚዎች ዝርዝር';
 $string['userbulk'] = 'የጅምላ የተጠቃሚ እርምጃዎች';
 $string['adduser'] = 'አዲስ ተጠቃሚ ጨምር';
 $string['uploadusers'] = 'ተጠቃሚዎችን በፋይል ስቀል';
+$string['uploaduserspreview'] = 'የተጠቃሚዎች ፋይል ቅድመ-ዕይታ';
+$string['userpolicies'] = 'የተጠቃሚ ፖሊሲዎች';
 $string['permissions'] = 'ፈቃዶች';
 $string['defineuserroles'] = 'ሚናዎችን ይግለጹ';
 $string['assignroles'] = 'ሚናዎችን መድብ';
+$string['checkpermissions'] = 'የስርዓት ፈቃዶችን ፈትሽ';
+$string['capability'] = 'ችሎታ/ፈቃድ';
 
 // Course administration
 $string['managecourses'] = 'ኮርሶችን እና ምድቦችን አስተዳድር';
@@ -42,6 +46,35 @@ $string['addcategory'] = 'ምድብ ጨምር';
 $string['addnewcourse'] = 'አዲስ ኮርስ ጨምር';
 $string['restorecourse'] = 'ኮርስ መልስ';
 $string['coursetags'] = 'የኮርስ መለያዎች';
+$string['coursebackup'] = 'የኮርስ ምትኬ (Backup) ነባሪዎች';
+$string['courserestore'] = 'የኮርስ መልሶ ማቋቋም ነባሪዎች';
+$string['courseimport'] = 'የኮርስ ማስመጣት ነባሪዎች';
+$string['courserequest'] = 'የኮርስ ጥያቄዎች';
+$string['pendingrequests'] = 'በመጠባበቅ ላይ ያሉ የኮርስ ጥያቄዎች';
+
+// Grade administration
+$string['gradessettings'] = 'ጠቅላላ የውጤት ቅንብሮች';
+$string['gradecategorysettings'] = 'የውጤት ምድብ ቅንብሮች';
+$string['gradeitemsettings'] = 'የውጤት ንጥል ቅንብሮች';
+$string['scales'] = 'የውጤት መለኪያዎች';
+$string['letters'] = 'የፊደል ውጤቶች';
+
+// Plugin administration
+$string['pluginsoverview'] = 'የተጨማሪዎች (ፕለጊኖች) አጠቃላይ እይታ';
+$string['installplugins'] = 'ተጨማሪዎችን (ፕለጊኖችን) ጫን';
+$string['checkforupdates'] = 'ማሻሻያዎችን ፈትሽ';
+$string['activitymodules'] = 'የእንቅስቃሴ ሞጁሎች';
+$string['manageactivities'] = 'እንቅስቃሴዎችን አስተዳድር';
+$string['authentication'] = 'የማረጋገጫ ዘዴዎች (Auth)';
+$string['manageauth'] = 'ማረጋገጫዎችን አስተዳድር';
+$string['enrolments'] = 'የምዝገባ ዘዴዎች (Enrol)';
+$string['manageenrols'] = 'የምዝገባ ዘዴዎችን አስተዳድር';
+$string['blocks'] = 'ብሎኮች';
+$string['manageblocks'] = 'ብሎኮችን አስተዳድር';
+$string['filters'] = 'ማጣሪያዎች';
+$string['managefilters'] = 'ማጣሪያዎችን አስተዳድር';
+$string['repositories'] = 'የፋይል ማከማቻዎች (Repositories)';
+$string['managerepositories'] = 'የፋይል ማከማቻዎችን አስተዳድር';
 
 // Appearance administration
 $string['themes'] = 'ገጽታዎች (ቴሞች)';
@@ -50,18 +83,46 @@ $string['themeswitcher'] = 'ገጽታ ቀይር';
 $string['navigation'] = 'የአሰሳ ቅንብሮች';
 $string['logos'] = 'አርማዎች (ሎጎዎች)';
 $string['frontpage'] = 'የመነሻ ገጽ ቅንብሮች';
+$string['frontpagesettings'] = 'የመነሻ ገጽ ቅንብሮች';
+$string['defaultuserprofileimage'] = 'ነባሪ የተጠቃሚ መገለጫ ምስል';
+$string['userprofile'] = 'የተጠቃሚ መገለጫ መስኮች';
 
 // Server administration
 $string['systeminformation'] = 'የስርዓቱ መረጃ';
 $string['environment'] = 'የአገልጋይ አካባቢ';
 $string['performance'] = 'አፈጻጸም';
 $string['security'] = 'ደህንነት';
+$string['sitepolicies'] = 'የጣቢያ ፖሊሲዎች';
 $string['http'] = 'ኤችቲቲፒ (HTTP)';
 $string['maintenance'] = 'የጥገና ሁነታ';
 $string['cleanup'] = 'ማጽዳት';
+$string['scheduledtasks'] = 'የታቀዱ ተግባራት (Tasks)';
+$string['tasklogs'] = 'የስራ ፍሰት ምዝግብ ማስታወሻዎች';
+$string['caching'] = 'የማህደረ ትውስታ (Cache) ውቅር';
+$string['purgeslashes'] = 'ማህደረ ትውስታዎችን አጽዳ';
+
+// Reports & Analytics
+$string['comments'] = 'አስተያየቶች';
+$string['backups'] = 'የምትኬ ሪፖርቶች';
+$string['eventlist'] = 'የክስተቶች ዝርዝር';
+$string['logs'] = 'የስርዓቱ ምዝግብ ማስታወሻዎች (Logs)';
+$string['loglive'] = 'የቀጥታ ምዝግብ ማስታወሻዎች';
+$string['overview'] = 'አጠቃላይ እይታ';
+$string['securityoverview'] = 'የደህንነት አጠቃላይ እይታ';
+$string['stats'] = 'ስታቲስቲክስ';
 
 // Language administration
 $string['language'] = 'ቋንቋ';
 $string['languagesettings'] = 'የቋንቋ ቅንብሮች';
 $string['languagepacks'] = 'የቋንቋ ጥቅሎች';
 $string['languagecustomization'] = 'የቋንቋ ማስተካከያ';
+
+// Advanced features
+$string['advancedfeatures'] = 'የላቁ ባህሪያት';
+$string['enablecompletion'] = 'የማጠናቀቂያ ክትትልን አንቃ';
+$string['enableavailability'] = 'የተገደበ መዳረሻን አንቃ';
+$string['enablebadges'] = 'ባጆችን አንቃ';
+$string['enablenotes'] = 'ማስታወሻዎችን አንቃ';
+$string['enablecomments'] = 'አስተያየቶችን አንቃ';
+$string['enabletags'] = 'መለያዎችን አንቃ';
+$string['enableanalytics'] = 'ትንታኔዎችን አንቃ';
